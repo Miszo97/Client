67078b2583b4939f0eb6aef76eb9999aecdb2c93
Artur <artsspe@gmail.com>
2018-05-18 01:29:24 +0200

    Add Client class.

---
+++ Client.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Artur Spek on 18/05/2018.
+//
+
+#include "Client.hpp"
---
+++ Client.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Artur Spek on 18/05/2018.
+//
+
+#ifndef CLIENT_CLIENT_HPP
+#define CLIENT_CLIENT_HPP
+
+
+class Client {
+
+};
+
+
+#endif //CLIENT_CLIENT_HPP

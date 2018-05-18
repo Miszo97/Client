1dba667e5707882084fd644a3cc8bf2f056236f2
Artur <artsspe@gmail.com>
2018-05-18 18:37:33 +0200

    Add displayError slot implementation.

--- Client.cpp
+++ Client.cpp
@@ -3,6 +3,25 @@
 //
 
 #include "Client.hpp"
+#include "Request_Response.pb.h"
+#include <QtWidgets/QComboBox>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QtWidgets/QLabel>
+#include <QtNetwork/QNetworkSession>
+#include <QAbstractSocket>
+#include <QMessageBox>
+
+Client::Client(QWidget* parent) : QDialog(parent), hostCombo(new QComboBox), portLineEdit(new QLineEdit)
+                                  , getFortuneButton(new QPushButton(tr("Get Fortune"))), socket(new QTcpSocket(this)) {
+    socket->abort();
+    socket->connectToHost(hostCombo->currentText(), portLineEdit->text().toInt());
+
+    connect(socket, QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error),
+            this, &Client::displayError);
+
+
+}
 
 void Client::sendRequest() {
 
@@ -13,7 +32,25 @@ void Client::readResponse() {
 }
 
 void Client::displayError(QAbstractSocket::SocketError socketError) {
+    switch (socketError) {
+        case QAbstractSocket::RemoteHostClosedError:
+            break;
+        case QAbstractSocket::HostNotFoundError:
+            QMessageBox::information(this, tr("Fortune Client"), tr("The host was not found. Please check the "
+                                                                    "host name and port settings."));
+            break;
+        case QAbstractSocket::ConnectionRefusedError:
+            QMessageBox::information(this, tr("Fortune Client"), tr("The connection was refused by the peer. "
+                                                                    "Make sure the fortune server is running, "
+                                                                    "and check that the host name and port "
+                                                                    "settings are correct."));
+            break;
+        default:
+            QMessageBox::information(this, tr("Fortune Client"),
+                                     tr("The following error occurred: %1.").arg(socket->errorString()));
+    }
 
+    getFortuneButton->setEnabled(true);
 }
 
 void Client::enableGetFortuneButton() {
